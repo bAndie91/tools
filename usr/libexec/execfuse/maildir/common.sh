c01dd85ce5e1bd7c8acfee34944b2a92400a81ba
Andreas Hrubak <802732+bAndie91@users.noreply.github.com>
2021-01-11 13:43:31 +0100

    execfuse/maildirfs: support marking real files by "inode=..." in virtual file name

--- usr/libexec/execfuse/maildir/common.sh
+++ usr/libexec/execfuse/maildir/common.sh
@@ -10,10 +10,24 @@ EOPNOTSUPP=95
 
 virt2phy_filepath()
 {
-if [[ ! $1 =~ /.getmail/ && $1 =~ ^(.*/)([^/]+)\[(.+?)\]\.eml$ ]]
+local virtpath=$1
+local basedir=$2
+
+if [[ $1 =~ /.getmail/ ]]
 then
-	echo "${BASH_REMATCH[1]}${BASH_REMATCH[3]}"
-else
 	echo "$1"
+else
+	if [[ $1 =~ ^(.*/)([^/]+)\{inode=([0-9]+?)\}\.eml$ ]]
+	then
+		local pathprefix=${BASH_REMATCH[1]}
+		local inum=${BASH_REMATCH[3]}
+		fname=`find "$basedir$pathprefix" -maxdepth 1 -inum $inum -printf "%f"`
+		echo "$pathprefix$fname"
+	elif [[ $1 =~ ^(.*/)([^/]+)\[(.+?)\]\.eml$ ]]
+	then
+		echo "${BASH_REMATCH[1]}${BASH_REMATCH[3]}"
+	else
+		echo "$1"
+	fi
 fi
 }

26405cd0b4fb1a4d6c49b2d5dcaf1859748b1a0d
Andreas Hrubak <ad132454@uucp.hu>
2023-08-19 23:38:15 +0200

    tabdata: better docs, better --help handler, new member: td-keepheader

--- tabdata/common.pl
+++ tabdata/common.pl
@@ -6,12 +6,24 @@ $FS = "\t";
 %Header = ();
 $OptNoHeader = 0;
 
-use Getopt::Long qw/:config no_ignore_case/;
+use Getopt::Long qw/:config no_ignore_case pass_through require_order no_getopt_compat/;
+use Pod::Usage;
 
 GetOptions(
 	'H|no-header' => \$OptNoHeader,
-	'help' => sub { print "NAME\n  $0\n"; print $HelpText; exit 0; },
-) or die "Usage: $0 [-H | --no-header] ...\n";
+	'help|?' => sub{ pod2usage(-exitval=>0, -verbose=>99); },
+) or pod2usage(-exitval=>2, -verbose=>99);
+
+if('--' ~~ @ARGV and $ARGV[0] ne '--')
+{
+	# at least 1 unknown option remained in @ARGV
+	pod2usage(-exitval=>2, -verbose=>99, -msg=>"$0: unknown parameter: $ARGV[0]");
+}
+
+# when pass_through option is set:
+# Note that the options terminator (default "--"), if present, will also be passed through in @ARGV.
+shift @ARGV if $ARGV[0] eq '--';
+
 
 sub process_header
 {

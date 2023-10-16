4a521ff52b39dc5ecf9cba4496e8f1d80fbc8a97
Andreas Hrubak <ad132454@uucp.hu>
2023-10-16 15:00:59 +0200

    bash command-not-found-handler takes into account commands which start/end with the typed word exactly

--- bash/command-not-found-handle.sh
+++ bash/command-not-found-handle.sh
@@ -6,15 +6,17 @@ command_not_found_handle()
 	#shift
 	#declare -a args=("$@")
 	
-	echo -n "$command: command not found. other similar commands: " >&2
+	echo "$command: command not found. similar commands: " >&2
 	
-	local threshold
+	local distance_threshold
 	if [ ${#command} -le 3 ]
 	then
-		threshold=1
+		distance_threshold=1
 	else
-		threshold=2
+		distance_threshold=2
 	fi
+	local startswith_threshold=3
+	local endswith_threshold=0
 	
 	{
 		find ${PATH//:/ } -maxdepth 1 -type f -executable -printf '%f\n' 2>/dev/null
@@ -22,18 +24,34 @@ command_not_found_handle()
 		alias -p | cut -d' ' -f2- | cut -d= -f1
 	}\
 	| levenshtein-distance "$command" \
-	| while read -r metric word
-	do
-		if [ $metric -le $threshold ]
-		then
-			echo "$metric	$word"
-		fi
-	done \
-	| sort -n \
-	| sed -e 's/^\S\+\s\+//' \
-	| tr "\n" " " \
+	| env command=$command perl -ne '
+		use List::Util qw/min/;
+		chomp;
+		$\ = "\n";
+		$, = "\t";
+		s/^(\d+)\s//;
+		$distance = $1;
+		$lpos = index $_,  $ENV{command}; $lpos = 999 if $lpos == -1;
+		$rpos = rindex $_, $ENV{command}; $rpos = 999 if $rpos == -1;
+		print $lpos, $rpos, $distance, length($_), $_;' \
+	| env startswith_threshold=$startswith_threshold endswith_threshold=$endswith_threshold distance_threshold=$distance_threshold \
+		perl -ne '
+		/(\S+)\s(\S+)\s(\S+)/;
+		if($1 > $ENV{startswith_threshold} and $2 > $ENV{endswith_threshold} and $3 > $ENV{distance_threshold}) {}
+		else { print; }
+		' \
+	| perl -ne '
+		use List::Util qw/min/;
+		$, = "\t";
+		s/(\S+)\s(\S+)\s(\S+)\s//;
+		@scores = ($1, $2, $3);
+		print min(@scores), $_;' \
+	| sort -k1n -k2n \
+	| cut -d$'\t' -f3- \
+	| column \
 	  >&2
-	echo >&2
+
+# TODO dont show too few/many suggestions
 	
 	return 127
 }

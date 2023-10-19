a64cc16b45be595784f312af0a069dd85af1ed9c
Andreas Hrubak <ad132454@uucp.hu>
2023-10-19 18:51:34 +0200

    tabdata tools more verbose error messages

---
+++ user-tools/Makefile.lists.mk
@@ -0,0 +1,27 @@
+TOOLS ?=   2opml adr2html asterisk-log-separator awk-cut base58 base64url \
+  bencode2json cdexec chattr-cow chattr-nocow chromium_cookie_decrypt.py \
+  chshebang convert_chromium_cookies_to_netscape.sh corner_time \
+  cut.awk daemonctl dataurl2bin debdiff descpids dfbar digasn diu dlnew \
+  dump_php_session.php eat errorlevel error_reporting.php fcomplete \
+  fc-search-codepoint fdupes-hardlink ff fgat find-by-date findnewestfile \
+  findoldestfile fixlogfiledatetime fixRFC822filemtime fmtkv foreach \
+  g_filename_to_uri git-checkout-sparse gitdiff grepdatetime Head \
+  header.sed indent2tree inisort is_gzip jobsel json2bencode json2msgpack \
+  kt LevelDB lines screens lnto locale-validator lpset ls2html lsata \
+  lsenv mime_extract mkdeb moz_bookmarks msg msgpack2json multithrottler \
+  mysql-fix-orphan-privileges.php noacute nocomment nthash.py ocspverify \
+  organizebydate palemoon-current-url parsel partial paths2indent \
+  perl-repl pipekill PMbwmon PMdirindex PMhexdiff PMnslist PMpwgen \
+  PMrecdiff pyzor-files qrwifi pvalve renamemanual repeat \
+  reportcmdstatus rsacrypt rsync-semichroot rsysrq screenconsole \
+  screen-notify screenreattach sockio ssh-agent-finder \
+  straceall strip-ansi-seq swap symlinks2dot tabularize Tail takeown taslis \
+  tests text2img-dataurl touchx ttinput uchmod unicodestyle upsidedown \
+  url_decode url_encode url_encode_bf url-parts vidir-sanitize vifiles \
+  waitpid whisper-retention-info wikibot xdg-autostart xml2json \
+  levenshtein-distance jaro-metric jaro-winkler-metric loggerexec \
+  logrotate-counters logto pdfflop pngmetatext set-sys-path subst_sudo_user \
+  terminaltitle timestamper set-xcursor-lock-and-run mkmagnetlink htmlentities \
+  pfx2pem rdfindreport symlinks-analyze rn filterexec trackrun oded cled \
+  mail-extract-addresses mail-extract-raw-headers fzf-preview w3m-render \
+  randstr rcmod killp killpgrp killcmd killexe hlcal hlncal

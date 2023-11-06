1570ef2899337de3271fab31f5c784f467950230
Andreas Hrubak <ad132454@uucp.hu>
2023-11-06 12:41:28 +0100

    add verbose flag to remove

--- compiled-tools/remove.c
+++ compiled-tools/remove.c
@@ -3,19 +3,31 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <libintl.h>
+#include <locale.h>
+#include <stdbool.h>
 
-static const char* helptext = "Usage: remove <PATH> [<PATH> [...]]\n"
+static const char* helptext = "Usage: remove [-v] <PATH> [<PATH> [...]]\n"
 	"Remove files and directories alike.\n";
 
 int main(int argc, char** argv)
 {
 	int fail;
 	int idx = 0;
+	bool verbose = false;
+	
+	setlocale(LC_ALL, "");
 	
 	if(argc == 2 && strcmp(argv[1], "--help")==0)
 	{
 		printf(helptext);
 		return 0;
+	}
+	
+	if(argc >= 2 && (strcmp(argv[1], "-v")==0 || strcmp(argv[1], "--verbose")==0))
+	{
+		idx++;
+		verbose = true;
 	}
 	
 	if(argc < 2)
@@ -27,7 +39,7 @@ int main(int argc, char** argv)
 	while(argv[++idx] != NULL)
 	{
 		fail = remove(argv[idx]);
-		if(fail == 0) continue;
-		err(errno, "%s", argv[idx]);
+		if(fail != 0) err(errno, "%s", argv[idx]);
+		if(verbose) warnx(dcgettext("coreutils", "%s: removed", LC_MESSAGES), argv[idx]);
 	}
 }

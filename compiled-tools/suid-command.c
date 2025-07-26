e1b1e3b69cb2f88e02d49fbe10c1b1b15e3a94f1
Andreas Hrubak <ad132454@uucp.hu>
2025-07-27 01:46:00 +0200

    allow calling as logon shell

--- compiled-tools/suid-command.c
+++ compiled-tools/suid-command.c
@@ -9,7 +9,13 @@ int main(int argc, char* argv[])
 	char * slash = strrchr(argv[0], '/');
 	if(slash != NULL)
 	{
+		/* allow calling this programm by path */
 		argv[0] = slash+1;
+	}
+	if(argv[0][0] == '-')
+	{
+		/* in case of this programm is called as a logon shell */
+		argv[0] += 1;
 	}
 	if(strlen(argv[0]) <= 5 || strncmp(argv[0], "suid-", 5)!=0)
 	{

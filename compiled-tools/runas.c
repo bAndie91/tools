70b3c805d0f9d53dd91c6ca0ba54b348b4b9d6f3
Andreas Hrubak <ad132454@uucp.hu>
2025-08-17 12:21:11 +0200

    runas can keep some envs

--- compiled-tools/runas.c
+++ compiled-tools/runas.c
@@ -13,6 +13,15 @@
 #define USERNAME (argv[1])
 #define COMMAND (argv[2])
 
+#define DELIMITED_STR_LOOP_BEGIN(delimited_str_ptr, delimiter_ptr, delimiter_chr) \
+	while(delimited_str_ptr != NULL && delimited_str_ptr[0] != '\0') { \
+		delimiter_ptr = strchr(delimited_str_ptr, delimiter_chr);
+
+#define DELIMITED_STR_LOOP_END(delimited_str_ptr, delimiter_ptr, delimiter_chr) \
+		if(delimiter_ptr == NULL) break; \
+		delimited_str_ptr = delimiter_ptr + 1; }
+
+
 char * new_string(const char * fmt, ...)
 {
 	int ok;
@@ -66,17 +75,34 @@ int main(int argc, char *argv[])
 		error(errno, errno, "setuid");
 	}
 	
-	char ** envs = calloc(8, sizeof(char*));
+	int num_keep_envs = 0;
+	char * keep_envs;
+	char * comma;
+	keep_envs = getenv("RUNAS_KEEP_ENVS");
+	DELIMITED_STR_LOOP_BEGIN(keep_envs, comma, ',')
+		num_keep_envs++;
+	DELIMITED_STR_LOOP_END(keep_envs, comma, ',')
+	
+	int env_idx = 0;
+	char ** envs = calloc(8 + num_keep_envs, sizeof(char*));
 	if(envs == NULL) { abort(); }
 	
-	envs[0] = new_string("PATH=%s", getenv("PATH"));
-	envs[1] = new_string("HOME=%s", pw->pw_dir);
-	envs[2] = new_string("SHELL=%s", pw->pw_shell);
-	envs[3] = new_string("USER=%s", USERNAME);
-	envs[4] = new_string("LOGNAME=%s", USERNAME);
-	envs[5] = envs[6] = envs[7] = NULL;
-	if(getenv("TERM") != NULL)     { envs[5] = new_string("TERM=%s",    getenv("TERM")); }
-	if(getenv("TERMCAP") != NULL)  { envs[6] = new_string("TERMCAP=%s", getenv("TERMCAP")); }
+	envs[env_idx++] = new_string("PATH=%s", getenv("PATH"));
+	envs[env_idx++] = new_string("HOME=%s", pw->pw_dir);
+	envs[env_idx++] = new_string("SHELL=%s", pw->pw_shell);
+	envs[env_idx++] = new_string("USER=%s", USERNAME);
+	envs[env_idx++] = new_string("LOGNAME=%s", USERNAME);
+	if(getenv("TERM") != NULL)     { envs[env_idx++] = new_string("TERM=%s",    getenv("TERM")); }
+	if(getenv("TERMCAP") != NULL)  { envs[env_idx++] = new_string("TERMCAP=%s", getenv("TERMCAP")); }
+	
+	keep_envs = getenv("RUNAS_KEEP_ENVS");
+	char * envname;
+	DELIMITED_STR_LOOP_BEGIN(keep_envs, comma, ',')
+		envname = new_string("%.*s", comma - keep_envs, keep_envs);
+		if(getenv(envname) != NULL) envs[env_idx++] = new_string("%s=%s", envname, getenv(envname));
+	DELIMITED_STR_LOOP_END(keep_envs, comma, ',')
+	
+	envs[env_idx] = NULL;
 	
 	errno = 0;
 	execvpe(COMMAND, &COMMAND, envs);
